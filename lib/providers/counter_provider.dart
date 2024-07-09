b7b3e702a6975f898e57bbde56a70c9a7484bb13
Mink <lienhung75757575@gmail.com>
2024-07-09 10:58:06 +0700

    add counterProvider

---
+++ lib/providers/counter_provider.dart
@@ -0,0 +1,21 @@
+
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'counter_provider.g.dart';
+
+@riverpod
+class CounterNotifier extends _$CounterNotifier {
+
+  @override
+  int build() {
+    return 0;
+  }
+
+  void increment() {
+    state++;
+  }
+
+  void decrement() {
+    state--;
+  }
+}
